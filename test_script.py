092cb109aa8a0ff2c85aca799073b8725ee29573
Shawn Fortin <Shawn Fortin>
2017-03-21 12:55:06 -0400

    Fixes a tiny mistake in one of the comments.

--- test_script.py
+++ test_script.py
@@ -2,7 +2,7 @@ from TwitterSearch import *
 try:
     tso = TwitterSearchOrder() # create a TwitterSearchOrder object
     tso.set_keywords(['Giveaway']) # let's define all words we would like to have a look for
-    tso.set_language('en') # we want to see German tweets only
+    tso.set_language('en') # we want to see English tweets only
     tso.set_include_entities(False) # and don't give us all those entity information
 
     # it's about time to create a TwitterSearch object with our secret tokens
