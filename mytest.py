f11bd6ae20cc605319ee55ea9f3cfe45b0c1985d
Connor Mendell <zenith123138@gmail.com>
2017-03-23 16:20:56 -0400

    Fixes #18
    This allows for dynamically building searches. HOWEVER we need to
    integrate this into the UI somehow

--- mytest.py
+++ mytest.py
@@ -34,71 +34,89 @@ def enter_giveaway(api, status):
     if 'favorite' in str(status_str):
         api.CreateFavorite(None, status_id[1])
         
+def custom_filter(user_filter):
+    split_filter = user_filter.split(' ')
+    size = len(split_filter)
+    new_string = ''
+    for i in range(size):
+        if split_filter[i][0] == '#':
+            split_filter[i] = '%23' + split_filter[i][1:]
+    i = 0
+    while (i < size):
+        new_string += split_filter[i]
+        new_string += '%20'
+        i += 1
+    query = "l=en&q=Giveaway%20" + new_string + "since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100"
+    return query
+
 
 # Create an API object to interact with the Twitter API
 api = twitter.Api(
-    consumer_key = 'JkLa7tiZDkRkEHAVs9rRs8q5z',
-    consumer_secret = '5uNENyyQVLsJxANWBG57gWVjfIWwTxuRKMMEvpbHds754rqWjS',
-    access_token_key = '3442532363-awZtxIgpTv3dpXUbjyb0NXjTk8YWYdvcljZigor',
-    access_token_secret = 't005Jq27nOqXfTQcE7AYfy5Y0q6ASQ9xYqU1mUsQdRrQb'
+    consumer_key = 'cfNpeV5za47PJBmjm9GrjoQ3F',
+    consumer_secret = 'rxIa3R1nwPQSOwSHMCwlE0xOGfHqrDSnsvqJMFPFnnLc2CpXQA',
+    access_token_key = '737864657659105280-fJpSG7fDSMW77jCBAmDMEkhpWBReRz8',
+    access_token_secret = 'LQjiiaKLRKeXFR6Ew0aaSIsSgb2CczPLIox9rG9FjA5ea'
 )
       
 #print(api.VerifyCredentials())
 
 results = {}
+user_filter = 'macbook #film'
+search_custom = custom_filter(user_filter)
+results['Custom Filter'] = search_custom
 search_default = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['default'] = search_default
 search_game = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20game%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20game%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['game'] = search_game
 search_beauty = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20beauty%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20beauty%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['beauty'] = search_beauty
 search_csgo = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20csgo%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20csgo%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['csgo'] = search_csgo
 search_stattrak = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20StatTrak%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20StatTrak%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['StatTrak'] = search_stattrak
 search_desktop = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20Desktop%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20Desktop%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Desktop'] = search_desktop
 search_shirt = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20shirt%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20shirt%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['shirt'] = search_shirt
 search_ps4 = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20ps4%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20ps4%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['ps4'] = search_ps4
 search_xboxone = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20xbox%20one%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20xbox%20one%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Xbox One'] = search_xboxone
 search_pc = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20PC%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20PC%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['pc'] = search_pc
 search_knife = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20knife%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20knife%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Knives'] = search_knife
 search_iphone = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20iphone%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20iphone%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['iphone'] = search_iphone
 search_console = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20console%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20console%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Console'] = search_console
 search_logitech = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20logitech%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20logitech%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Logitech'] = search_logitech
 search_nvidia = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20Nvidia%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20Nvidia%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Nvidia'] = search_nvidia
 search_hashtag_giveaway = api.GetSearch(
-    raw_query="l=en&q=%23Giveaway%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=%23Giveaway%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['#Giveaway'] = search_hashtag_giveaway
 search_itunes = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20Itunes%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20Itunes%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Itunes'] = search_itunes
 search_googleplay = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20google%20play%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+    raw_query="l=en&q=Giveaway%20google%20play%20since%3A2017-03-22%20until%3A2017-03-23&src=typd&count=100")
 results['Google Play'] = search_googleplay
 
 # Enter all tweets found in search above
