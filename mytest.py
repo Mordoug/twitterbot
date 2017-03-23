03b6156e1b596b7deaf31f1539f6d093fecb408b
Connor Mendell <zenith123138@gmail.com>
2017-03-23 00:44:19 -0400

    Fixes #10
    Adds a whole bunch of search filters and throws them into a dictionary
    for later use. If necessary i can split up dictionary entries into
    individual pieces however as it stands im not sure that would be the
    right idea.

--- mytest.py
+++ mytest.py
@@ -36,13 +36,65 @@ api = twitter.Api(
       
 #print(api.VerifyCredentials())
 
-
-results = api.GetSearch(
-    raw_query="l=en&q=Giveaway%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")  
+results = {}
+search_default = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['default'] = search_default
+search_game = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20game%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['game'] = search_game
+search_beauty = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20beauty%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['beauty'] = search_beauty
+search_csgo = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20csgo%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['csgo'] = search_csgo
+search_stattrak = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20StatTrak%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['StatTrak'] = search_stattrak
+search_desktop = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20Desktop%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Desktop'] = search_desktop
+search_shirt = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20shirt%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['shirt'] = search_shirt
+search_ps4 = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20ps4%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['ps4'] = search_ps4
+search_xboxone = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20xbox%20one%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Xbox One'] = search_xboxone
+search_pc = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20PC%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['pc'] = search_pc
+search_knife = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20knife%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Knives'] = search_knife
+search_iphone = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20iphone%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['iphone'] = search_iphone
+search_console = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20console%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Console'] = search_console
+search_logitech = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20logitech%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Logitech'] = search_logitech
+search_nvidia = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20Nvidia%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Nvidia'] = search_nvidia
+search_hashtag_giveaway = api.GetSearch(
+    raw_query="l=en&q=%23Giveaway%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['#Giveaway'] = search_hashtag_giveaway
+search_itunes = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20Itunes%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Itunes'] = search_itunes
+search_googleplay = api.GetSearch(
+    raw_query="l=en&q=Giveaway%20google%20play%20since%3A2017-03-20%20until%3A2017-03-21&src=typd&count=100")
+results['Google Play'] = search_googleplay
 
 #print ("%s", (results))
 
-for tweet in results:
+for tweet in search_default:
     try:
         enter_giveaway(api, tweet)
     except (twitter.TwitterError):
